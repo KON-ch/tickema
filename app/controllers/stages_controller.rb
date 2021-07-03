31b88efc11ebe7fe96ad80cd0027a2177b8f3847
Kazuki Kondo <k.kazuki526@gmail.com>
2021-07-03 23:14:27 +0900

    create show action stage

--- app/controllers/stages_controller.rb
+++ app/controllers/stages_controller.rb
@@ -1,6 +1,11 @@
 class StagesController < ApplicationController
   def index
-    stages = Stage.select(:title)
+    stages = Stage.select(:id, :title)
     render json: stages
+  end
+
+  def show
+    stage = Stage.find(params[:id])
+    render json: stage
   end
 end
