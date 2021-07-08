efe3ce9766e7440fa3a2f7f5015ad4e0a80c8bf9
Kazuki Kondo <k.kazuki526@gmail.com>
2021-07-09 00:05:25 +0900

    create test top index

--- spec/requests/top_spec.rb
+++ spec/requests/top_spec.rb
@@ -1,4 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe "Tops", type: :request do
+  describe "GET #index " do
+    it "status 200" do
+      get "/"
+      expect(response).to have_http_status(200)
+    end
+  end
 end
