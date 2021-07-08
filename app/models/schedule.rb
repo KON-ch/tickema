e58c652716699aa5ff0d43c1b47c12354a08b367
Kazuki Kondo <k.kazuki526@gmail.com>
2021-07-09 00:02:42 +0900

    add association stage schedule

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,4 +1,8 @@
 class Schedule < ApplicationRecord
+  has_many :stages, through: :stage_schedules
+  has_many :stage_schedules, dependent: :destroy
+  accepts_nested_attributes_for :stage_schedules
+
   validates :start_time, presence: true
   validates :staging_date, presence: true
 end
