40bac299ef7cddcea7bbf61f8099e6d7b8a78b68
Kazuki Kondo <k.kazuki526@gmail.com>
2021-07-10 19:58:56 +0900

    fix model association

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,7 +1,6 @@
 class Schedule < ApplicationRecord
-  has_many :stages, through: :stage_schedules
   has_many :stage_schedules, dependent: :destroy
-  accepts_nested_attributes_for :stage_schedules
+  has_many :stages, through: :stage_schedules
 
   validates :start_time, presence: true
   validates :staging_date, presence: true
--- app/models/stage.rb
+++ app/models/stage.rb
@@ -1,6 +1,6 @@
 class Stage < ApplicationRecord
+  has_many :stage_schedules, dependent: :destroy
   has_many :schedules, through: :stage_schedules
-  has_many :stage_schedules
 
   validates :title, presence: true, length: { maximum: 20 }
 end
