5201b018988a253bb5b7c3e57bb2be4a0958b9ee
Kazuki Kondo <k.kazuki526@gmail.com>
2021-07-17 23:55:54 +0900

    create schedule new page

--- app/javascript/ScheduleNewPage.vue
+++ app/javascript/ScheduleNewPage.vue
@@ -5,11 +5,43 @@
         <li><font color="red">{{ e }}</font></li>
       </ul>
     </div>
+    <div>
+      <label>開始日</label>
+      <input v-model="startDate" type="date">
+      <label>終了日</label>
+      <input v-model="endDate" type="date">
+      <label>開演時間</label>
+      <input v-model="startTime" type="time">
+    </div>
+    <table>
+      <thead>
+        <tr>
+          <th></th>
+          <th>{{ startTime }}</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>{{ startDate }}</td>
+          <td>
+            <input type="checkbox">
+          </td>
+        </tr>
+        <tr>
+          <td>{{ endDate }}</td>
+          <td>
+            <input type="checkbox">
+          </td>
+        </tr>
+      </tbody>
+    </table>
     <div v-for="schedule in schedules" :key="schedule.id">
       <label>日付</label>
       <input v-model="schedule.staging_date" type="date">
       <label>時間</label>
       <input v-model="schedule.start_time" type="time">
+    </div>
+    <div>
       <button type="submit">登録</button>
     </div>
   </form>
@@ -24,8 +56,14 @@
         schedules: [{
           staging_date: '',
           start_time: '',
+        },{
+          staging_date: '',
+          start_time: '',
         }],
         stage_id: this.$route.params.id,
+        startDate: '',
+        endDate: '',
+        startTime: '',
         errors: ''
       }
     },
