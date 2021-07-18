bab7bce86ed71c2b7e37f127bd9a811f91efb9b7
Kazuki Kondo <k.kazuki526@gmail.com>
2021-07-18 15:43:08 +0900

    add relation stage schedule to customer

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,6 @@
 class Customer < ApplicationRecord
+  has_many :stage_customers, dependent: :destroy
+  has_many :stages, through: :stage_customers
+
   validates :name, presence: true, length: { maximum: 10 }
 end
--- app/models/stage_schedule.rb
+++ app/models/stage_schedule.rb
@@ -1,6 +1,8 @@
 class StageSchedule < ApplicationRecord
   belongs_to :stage
   belongs_to :schedule
+  has_many :stage_customers, dependent: :destroy
+  has_many :customers, through: :stage_customers
 
   validates :stage_id, presence: true
   validates :schedule_id, presence: true
