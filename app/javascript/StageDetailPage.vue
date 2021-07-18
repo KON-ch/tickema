9382cfc6bb1e4a00a102ad54d0a2c9e1ad66f3ef
Kazuki Kondo <k.kazuki526@gmail.com>
2021-07-18 12:26:15 +0900

    fix stage detail page to schedule list

--- app/javascript/StageDetailPage.vue
+++ app/javascript/StageDetailPage.vue
@@ -1,11 +1,20 @@
 <template>
   <div>
     <h1>{{ stage.title }}</h1>
-    <div>
-      <ul v-for="schedule in stage.schedules" :key="schedule.id">
-        <li>{{ schedule.staging_date }}{{ schedule.start_time }}</li>
-      </ul>
-    </div>
+    <v-list>
+      <v-list-group v-for="schedule in stage.schedules" :key="schedule.id">
+        <template v-slot:activator>
+          <v-list-item-title>
+            {{ schedule.staging_date }}{{ schedule.start_time }}
+          </v-list-item-title>
+        </template>
+        <v-list-item>
+          <v-list-item-content>
+              hello
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-group>
+    </v-list>
   </div>
 </template>
 
