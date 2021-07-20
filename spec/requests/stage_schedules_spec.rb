876c37d9cd6c6611c6cf54240a57742704375281
Kazuki Kondo <k.kazuki526@gmail.com>
2021-07-20 23:06:49 +0900

    create request spec stage schedules

---
+++ spec/requests/stage_schedules_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe "StageSchedules", type: :request do
+  describe "GET #show" do
+    it "公演日程から顧客情報が取得できること" do
+      stage = FactoryBot.create(:stage)
+      schedule = FactoryBot.create(:schedule)
+      stage_schedule = FactoryBot.create(:stage_schedule)
+      customer = FactoryBot.create(:customer)
+      stage_customer = FactoryBot.create(:stage_customer)
+      get "/stage_schedules/#{stage_schedule.id}"
+      json = JSON.parse(response.body)
+      expect(response).to have_http_status(200)
+      expect(json[0]["name"]).to eq(customer.name)
+    end
+  end
+end
