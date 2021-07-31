6c2b5e78de18e3eb848fa21a8ff61e7de78500fa
Kazuki Kondo <k.kazuki526@gmail.com>
2021-07-31 15:15:17 +0900

    install devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # https://guides.rubyonrails.org/routing.html
 
+  devise_for :users
+
   root 'top#index'
+
   resources :stages, except: %i[new edit]
   resources :schedules, only: :create
   resources :customers, except: %i[new edit]
