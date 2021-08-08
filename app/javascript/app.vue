a44fb71255466560241258f82314a74620feb159
Kazuki Kondo <k.kazuki526@gmail.com>
2021-08-08 09:43:18 +0900

    fix vue router only stage detail

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -9,34 +9,15 @@ import Vue             from 'vue'
 import VueRouter       from 'vue-router'
 
 import UserPage        from './UserPage.vue'
-import StageIndexPage  from './StageIndexPage.vue'
 import StageDetailPage from './StageDetailPage.vue'
-import StageNewPage    from './StageNewPage.vue'
-import StageEditPage   from './StageEditPage.vue'
-import ScheduleNewPage from './ScheduleNewPage.vue'
-import CustomerNewPage from './CustomerNewPage.vue'
 
 const router = new VueRouter({
   routes: [
     { path: `/`,
       component: UserPage        },
-    { path: '/stages',
-      component: StageIndexPage  },
     { path: '/stages/:id(\\d+)',
       name: 'StageDetailPage',
       component: StageDetailPage },
-    { path: '/stages/new',
-      name: 'StageNewPage',
-      component: StageNewPage    },
-    { path: '/stages/:id(\\d+)/edit',
-      name: 'StageEditPage',
-      component: StageEditPage   },
-    { path: '/schedules/:id(\\d+)',
-      name: 'ScheduleNewPage',
-      component: ScheduleNewPage },
-    { path: '/customers/new',
-      name: 'CustomerNewPage',
-      component: CustomerNewPage },
   ]
 })
 
