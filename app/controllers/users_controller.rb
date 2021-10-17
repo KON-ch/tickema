a7be71c307e85728da825b8221ac1866e9eba400
Kazuki Kondo <k.kazuki526@gmail.com>
2021-10-17 14:47:07 +0900

    fix stage list order created_at desc

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
-    stage = Stage.select(:id, :title)
+    stage = Stage.select(:id, :title).order(created_at: :desc)
     render json: stage
   end
 end
