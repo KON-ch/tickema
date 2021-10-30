8b6801861f30f0d405391fbda013ca5d6fb5ea50
Kazuki Kondo <k.kazuki526@gmail.com>
2021-10-30 16:51:46 +0900

    add stage customer after save create ticket

--- app/models/stage_customer.rb
+++ app/models/stage_customer.rb
@@ -5,11 +5,17 @@ class StageCustomer < ApplicationRecord
   validates :stage_schedule_id, presence: true
   validates :customer_id, presence: true
 
+  after_save :create_ticket
   after_destroy :destroy_customer
 
   private
 
   def destroy_customer
     customer.destroy! if customer.stage_schedules.blank?
+  end
+
+  def create_ticket
+    stage_schedule = StageSchedule.find(stage_schedule_id)
+    Ticket.create(stage_id: stage_schedule.stage_id, schedule_id: stage_schedule.schedule_id, customer_id: customer_id)
   end
 end
