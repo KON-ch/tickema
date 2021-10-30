c029f62e97650797e97b8c1d40961836d7811ac8
Kazuki Kondo <k.kazuki526@gmail.com>
2021-10-30 17:38:01 +0900

    add create contact after save ticket

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -4,4 +4,13 @@ class Ticket < ApplicationRecord
   belongs_to :customer
 
   has_one :contact, dependent: :destroy
+
+  after_save :_create_contact
+
+  private
+
+  def _create_contact
+    user_id = customer.user.id
+    create_contact(user_id: user_id)
+  end
 end
