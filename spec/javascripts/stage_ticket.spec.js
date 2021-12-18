5ad94974056b482a29e884ceed19332070a1984a
Kazuki Kondo <k.kazuki526@gmail.com>
2021-12-18 15:45:48 +0900

    setup jest for vue

---
+++ spec/javascripts/stage_ticket.spec.js
@@ -0,0 +1,11 @@
+import { shallowMount } from '@vue/test-utils'
+import StageTicket from 'StageTicket';
+
+describe('StageTicket', () => {
+  it('test', () => {
+    expect(1 + 1).toBe(2)
+  });
+  it('passed test', () => {
+    expect(1 + 1).toBe(3)
+  });
+})
