f7e6cb73d39160a7774631691ddb450a49210601
Kazuki Kondo <k.kazuki526@gmail.com>
2022-01-03 11:49:17 +0900

    fix config to use vuetify in jest

--- spec/javascripts/jest.config.js
+++ spec/javascripts/jest.config.js
@@ -1,1 +1,18 @@
-
+module.exports = {
+  "roots": ["./"],
+  "moduleDirectories": [
+    "node_modules",
+    "app/javascript"
+  ],
+  "moduleFileExtensions": [
+    "js",
+    "json",
+    "vue"
+  ],
+  "transform": {
+    "^.+\\.js$": "../../node_modules/babel-jest",
+    ".*\\.(vue)$": "../../node_modules/vue-jest"
+  },
+  "testEnvironment": "jsdom",
+  "setupFiles": ["./setup.js"],
+}
