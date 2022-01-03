7cac4ad0593a8cf99addb9eb661243ee6325a1de
Kazuki Kondo <k.kazuki526@gmail.com>
2022-01-03 12:53:07 +0900

    add computed test to stage ticket component

--- spec/javascripts/stage_ticket.spec.js
+++ spec/javascripts/stage_ticket.spec.js
@@ -1,18 +1,13 @@
 import { shallowMount } from "@vue/test-utils"
 import StageTicket from "StageTicket";
 
-describe("StageTicket", () => {
+describe("props", () => {
   const wrapper = shallowMount(StageTicket, {
     propsData: {
       tickets: [
         {
-          contact_id: 1,
-          count: 1,
-          customer_id: 1,
           customer_name: "テスト タロウ",
           date: "1月1日",
-          id: 1,
-          schedule_id: 1,
           status: "reserved",
           time: "18:30"
         }
@@ -36,5 +31,60 @@ describe("StageTicket", () => {
 
   it("予約ステータスが表示されていること", () => {
     expect(wrapper.text()).toMatch("予約済み")
+  });
+});
+
+describe("setStatusColor", () => {
+  const wrapper = shallowMount(StageTicket)
+
+  it("予約ステータスの色に変換されること", () => {
+    // StageTicket.computed.setStatusColor()では、引数を渡したテストができない
+    expect(wrapper.vm.setStatusColor("reserved")).toBe("indigo")
+    expect(wrapper.vm.setStatusColor("applied")).toBe("amber")
+    expect(wrapper.vm.setStatusColor("notified")).toBe("red")
+    expect(wrapper.vm.setStatusColor("finished")).toBe("teal")
+  });
+})
+
+describe("searchTickets", () => {
+  const tickets_data = [
+    { customer_name: "テスト タロウ" },
+    { customer_name: "テスト ジロウ" },
+  ]
+  it("一致する顧客名が取得できること", () => {
+    const localThis = {
+      tickets: tickets_data,
+      keyword: "テスト タロウ"
+    }
+
+    // toBe()ではオブジェクトまたは配列の値を確認できない
+    expect(StageTicket.computed.searchTickets.call(localThis)).toEqual(
+      [
+        { customer_name: "テスト タロウ" }
+      ]
+    )
+  });
+
+  it("未入力の場合は全ての顧客を取得すること", () => {
+    const localThis = {
+      tickets: tickets_data,
+      keyword: ""
+    }
+
+    expect(StageTicket.computed.searchTickets.call(localThis)).toEqual(
+      [
+        { customer_name: "テスト タロウ" },
+        { customer_name: "テスト ジロウ" }
+      ]
+    )
+  });
+
+  it("一致しない場合は場合は空の配列が返ること", () => {
+    const localThis = {
+      tickets: tickets_data,
+      keyword: "ジェスト"
+    }
+
+    expect(StageTicket.computed.searchTickets.call(localThis)).toEqual([])
   });
 })
