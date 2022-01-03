c30a7af978e2f6cb54e1b2ab02c46f5136270157
Kazuki Kondo <k.kazuki526@gmail.com>
2022-01-03 12:22:32 +0900

    add basic test to stage ticket component

--- spec/javascripts/stage_ticket.spec.js
+++ spec/javascripts/stage_ticket.spec.js
@@ -1,11 +1,40 @@
-import { shallowMount } from '@vue/test-utils'
-import StageTicket from 'StageTicket';
+import { shallowMount } from "@vue/test-utils"
+import StageTicket from "StageTicket";
 
-describe('StageTicket', () => {
-  it('test', () => {
-    expect(1 + 1).toBe(2)
+describe("StageTicket", () => {
+  const wrapper = shallowMount(StageTicket, {
+    propsData: {
+      tickets: [
+        {
+          contact_id: 1,
+          count: 1,
+          customer_id: 1,
+          customer_name: "テスト タロウ",
+          date: "1月1日",
+          id: 1,
+          schedule_id: 1,
+          status: "reserved",
+          time: "18:30"
+        }
+      ]
+    }
   });
-  it('passed test', () => {
-    expect(1 + 1).toBe(3)
+
+  it("名前が表示されていること", () => {
+    // wrapper.find(".customer-name").toBe("テスト タロウ")
+    // selectorを指定した方が厳密だが、テストが壊れやすくなる
+    expect(wrapper.text()).toMatch("テスト タロウ")
+  });
+
+  it("予約日が表示されていること", () => {
+    expect(wrapper.text()).toMatch("1月1日")
+  });
+
+  it("予約時間が表示されていること", () => {
+    expect(wrapper.text()).toMatch("18:30")
+  });
+
+  it("予約ステータスが表示されていること", () => {
+    expect(wrapper.text()).toMatch("予約済み")
   });
 })
