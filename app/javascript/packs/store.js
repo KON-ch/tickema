580366840e594e2c62146681391bf5584b3bc12c
Kazuki Kondo <k.kazuki526@gmail.com>
2022-01-20 00:55:37 +0900

    fix vuex strict mode

--- app/javascript/packs/store.js
+++ app/javascript/packs/store.js
@@ -4,8 +4,7 @@ import Vuex from "vuex"
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  // todo: 本番環境ではfalseにする
-  strict: true,
+  strict: process.env.NODE_ENV !== "production",
   state: {
     id: 0,
     title: "",
