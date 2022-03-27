8f2a2dda41c620e4a8cb4fde6b701d391b61aa5c
Kazuki Kondo <k.kazuki526@gmail.com>
2022-03-27 12:02:39 +0900

    fix stages_controller, add tickets and candidates

--- app/controllers/stages_controller.rb
+++ app/controllers/stages_controller.rb
@@ -1,34 +1,42 @@
 class StagesController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_status_404
 
+  def index
+    stages = current_user.stages.select(:id, :title)
+    render json: stages, status: 200
+  end
+
   def show
     stage = set_stage
 
     render json: {
-      id:                 stage.id,
-      title:              stage.title,
-      stages:             current_user.stages.select(:id, :title).where.not(id: stage.id).order(id: :desc),
-      schedules:          stage.schedules.select(:id, :staged_on, :staged_at),
-      tickets:            set_tickets,
-      unbooked_customers: set_unbooked_customers
+      id: stage.id,
+      title: stage.title,
+      schedules: stage.schedules.select(:id, :staged_on, :staged_at),
     }
+  end
+
+  # GET
+  def tickets
+    tickets =
+      Ticket.includes(:customer, :schedule, :reservation)
+            .for_stage(current_user.id, params[:id]).map do |ticket|
+        TicketSerializer.new(ticket)
+      end
+
+    render json: tickets, status: 200
+  end
+
+  # GET
+  def candidates
+    candidates = current_user.customers.select(:id, :name).not_reserved(params[:id])
+    render json: candidates, status: 200
   end
 
   private
 
   def set_stage
     Stage.find_by(id: params[:id])
-  end
-
-  def set_tickets
-    @user_customers = current_user.customers.select(:id, :name)
-    @tickets = Ticket.where(stage_id: params[:id].to_i).where(customer_id: @user_customers.ids)
-    @tickets.includes(%i[customer schedule contact]).map(&:serializable_hash)
-  end
-
-  def set_unbooked_customers
-    booked_customer_ids = @tickets.pluck(:customer_id).uniq
-    @user_customers.where.not(id: booked_customer_ids)
   end
 
   def render_status_404(exception)
