b0967a14e180de97343a13ce8c5ee3c3dca523de
Kazuki Kondo <k.kazuki526@gmail.com>
2022-03-27 12:01:10 +0900

    create ticket serializer

---
+++ app/serializers/ticket_serializer.rb
@@ -0,0 +1,13 @@
+class TicketSerializer
+  def initialize(ticket)
+    @id = ticket.id
+    @customer_id = ticket.customer_id
+    @cusotmer_name = ticket.customer_name
+    @schedule_id = ticket.schedule_id
+    @date = ticket.date
+    @time = ticket.time
+    @reservation_id = ticket.reservation.id
+    @count = ticket.reservation.count
+    @status = ticket.reservation.status
+  end
+end
