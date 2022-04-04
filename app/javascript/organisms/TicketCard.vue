76292eb4ef9eb1fbe5ab6425427130f602b9c98b
Kazuki Kondo <k.kazuki526@gmail.com>
2022-04-04 23:34:55 +0900

    チケットコンポーネントのデザインを修正

--- app/javascript/organisms/TicketCard.vue
+++ app/javascript/organisms/TicketCard.vue
@@ -1,19 +1,23 @@
 <template>
   <v-card class="ticket">
-    <trash-button :id="ticket.id" />
-
-    <v-card-text>
-      {{ticket.date}}{{ticket.time}}
-    </v-card-text>
-    <v-card-title class="name">
-      {{ ticket.customer_name }}
-    </v-card-title>
-
     <v-row>
-      <v-col>
+      <v-col cols="2" class="trash-button">
+        <trash-button :id="ticket.id" />
+      </v-col>
+      <v-col cols="9" class="date">
+        {{ticket.date}}{{ticket.time}}
+      </v-col>
+    </v-row>
+
+    <div class="name">
+      {{ ticket.customer_name }}
+    </div>
+
+    <v-row class="infomation">
+      <v-col cols="6" class="status">
         <status-selecter :id="ticket.id" :status="ticket.status" />
       </v-col>
-      <v-col>
+      <v-col cols="5" class="count">
         <ticket-counter :id="ticket.id" :count="ticket.count" />
       </v-col>
     </v-row>
@@ -42,8 +46,42 @@ export default {
 </script>
 
 <style scoped>
+  .ticket {
+    border: solid gray;
+    outline: 1px solid gray;
+    outline-offset: -8px;
+  }
+
+  .trash-button {
+    margin-top: 8px;
+    margin-left: 8px;
+  }
+
+  .date {
+    margin: 32px 0 0;
+  }
+
   .name {
     text-align: center;
-    display: block;
+    margin-top: 24px;
+    font-size: 24px;
+  }
+
+  .infomation {
+    margin-top: 8px;
+  }
+
+  .status {
+    margin-left: 16px;
+    padding-right: 0;
+  }
+
+  .count {
+    padding-left: 0;
+    margin-top: 8px;
+  }
+
+  .col {
+    max-height: 96px;
   }
 </style>
