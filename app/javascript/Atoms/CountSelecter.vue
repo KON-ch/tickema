070bc2ddadcf7a466d23adbdddd11d51c00c7fd4
Kazuki Kondo <k.kazuki526@gmail.com>
2022-04-07 00:29:38 +0900

    未予約者の為の枚数と日程のセレクターを作成

---
+++ app/javascript/Atoms/CountSelecter.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-select
+    @input="item => $emit('input', item)"
+    :value="value"
+    :items="countItems"
+    background-color="white"
+    dense
+    flat
+    label="枚数"
+    solo
+    suffix="枚"
+  />
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      countItems: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    }
+  },
+  props: {
+    value: {
+      type: Number,
+      default: 1
+    }
+  }
+}
+</script>
+
---
+++ app/javascript/Atoms/ScheduleSelecter.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-select
+    @input="item => $emit('input', item)"
+    :value="value"
+    :items="schedules"
+    :item-text="schedule => schedule.staged_on + ' ' + schedule.staged_at"
+    item-value="id"
+    background-color="white"
+    dense
+    flat
+    label="公演日時"
+    solo
+  />
+</template>
+
+<script>
+  import { mapState } from 'vuex';
+  export default {
+    props: {
+      value: {
+        type: Number,
+        default: 1
+      }
+    },
+    computed: {
+      ...mapState(["id", "schedules"])
+    }
+  }
+</script>
