86a2f7901ca4a9c17fda7196902ad79af19d4d0b
Kazuki Kondo <k.kazuki526@gmail.com>
2022-04-08 01:14:17 +0900

    顧客名を入力するフィールドを作成

---
+++ app/javascript/Atoms/NameField.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-text-field
+    :value="value"
+    dense
+    @input="value => $emit('input', value)"
+  >
+    <template v-slot:label>{{ labelText }}</template>
+  </v-text-field>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: String,
+      default: ""
+    },
+    labelText: {
+      type: String,
+      default: ""
+    }
+  }
+}
+</script>
