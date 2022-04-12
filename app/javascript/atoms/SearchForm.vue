3268b90fad0af882c5aa9b07471409e3b53dc9ec
Kazuki Kondo <k.kazuki526@gmail.com>
2022-04-12 23:11:09 +0900

    検索フォームのコンポーネントを作成

---
+++ app/javascript/atoms/SearchForm.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-text-field
+    :value="value"
+    class="search-form"
+    @input="value => $emit('input', value)"
+    prepend-inner-icon="mdi-account-search-outline"
+  >
+    <template v-slot:label>検索</template>
+  </v-text-field>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: String,
+      default: ""
+    }
+  }
+}
+</script>
+
+<style scoped>
+.search-form {
+  margin: 32px 24px;
+}
+</style>
+
